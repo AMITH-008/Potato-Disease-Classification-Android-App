d38aade103260265383266e5729c9990459191c2
AMITH-008 <amithkouti@gmail.com>
2023-04-25 20:09:13 +0530

    Declared SelectImage and UploadImage

--- app/src/main/java/com/company/potato_disease/MainActivity.java
+++ app/src/main/java/com/company/potato_disease/MainActivity.java
@@ -12,6 +12,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.ContactsContract;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -21,10 +23,13 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    Button videoCapture, imageCapture;
+    Button videoCapture, imageCapture, imageSelect, imageUpload;
 
     private  static final int CAMERA_REQUEST_CODE = 2;
 
@@ -36,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Uri videoPath;
     private File imageFile;
-
+    private String imagePath;
     private ImageView imageView;
 
     @Override
@@ -46,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
 
         videoCapture = findViewById(R.id.videoCapture);
         imageCapture = findViewById(R.id.imageCapture);
+        imageSelect = findViewById(R.id.imageSelect);
+        imageUpload = findViewById(R.id.imageUpload);
         imageView = findViewById(R.id.image);
 
         if(isCameraPresentInPhone()) {
@@ -59,14 +66,15 @@ public class MainActivity extends AppCompatActivity {
         imageCapture.setOnClickListener(cameraCaptureClicked);
         videoCapture.setOnClickListener(videoCaptureClicked);
 
+
     }
     public View.OnClickListener cameraCaptureClicked = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            if(ContextCompat.checkSelfPermission(view.getContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
-                Toast.makeText(view.getContext(), "Requires Camera Permission", Toast.LENGTH_SHORT).show();
-                return;
-            }
+//            if(ContextCompat.checkSelfPermission(view.getContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+//                Toast.makeText(view.getContext(), "Requires Camera Permission", Toast.LENGTH_SHORT).show();
+//                return;
+//            }
             Toast.makeText(view.getContext(), "Camera Capturing Started", Toast.LENGTH_SHORT).show();
             Log.i(TAG, "Camera Capture Started");
             captureImage();
@@ -82,6 +90,20 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(view.getContext(), "Video Capturing Started", Toast.LENGTH_SHORT).show();
             Log.i(TAG, "Video Capture Started");
             recordVideo();
+        }
+    };
+
+    public View.OnClickListener selectImageClicked = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            selectImage();
+        }
+    };
+
+    public View.OnClickListener uploadImageClicked = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            uploadImage();
         }
     };
 
@@ -107,10 +129,11 @@ public class MainActivity extends AppCompatActivity {
             if(resultCode == RESULT_OK) {
                 try{
 
-                    //Log.i(TAG, "Video Is Recorded and available at path: "+ imagePath);
-                    imageFile = createFile();
+                    Log.i(TAG, "Image is captured and available at path: "+ imagePath);
+
                     Bitmap b = (Bitmap) data.getExtras().get("data");
                     imageView.setImageBitmap(b);
+
                 }catch (Exception e){
                     Log.i(TAG, e.getMessage());
                     return;
@@ -122,6 +145,15 @@ public class MainActivity extends AppCompatActivity {
                 Log.i(TAG, "Error Occurred");
             }
         }
+    }
+
+    private File createFile() throws IOException {
+        String time  = new SimpleDateFormat("ddMMyyyy_HHmmss").format(new Date());
+        String filename = "image_"+time+"_";
+        File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(filename, ".jpg", storageDirectory);
+        Log.i(TAG, "createFile: "+ image.toString());
+        return image;
     }
 
     private boolean isCameraPresentInPhone() {
@@ -140,7 +172,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void captureImage() {
+        try{
+            imageFile = createFile();
+        }catch (IOException e){
+            Log.i(TAG, "CaptureImage "+e.getMessage());
+        }
+
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(imageFile != null){
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imageFile));
+            imagePath = "file:"+imageFile.getAbsolutePath();
+        }
         startActivityForResult(intent, IMAGE_CAPTURE_CODE);
     }
+
+    private void selectImage() {
+
+    }
+
+    private void uploadImage() {
+
+    }
+
 }
